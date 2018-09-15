a654ce680dc66bab3f40c492c7a9a84a9d2f56ed
zhangrongmou <zhangrongmou@kuaishou.com>
2018-09-15 19:35:04 +0800

    增加config配置项目，实现远程管理config

--- bin/cli.js
+++ bin/cli.js
@@ -2,11 +2,11 @@
 
 'use strict';
 const path = require('path');
-const fs = require('fs');
 const program = require('commander');
-const chalk = require('chalk');
 const choice = require('../lib/choice');
 const pkg = require(path.join(__dirname, '../package.json'));
+const shell = require('shelljs');
+const configModel = require('../lib/config');
 
 program
     .version(pkg.version, '-v, --version')
@@ -15,8 +15,23 @@ program
     .command('init')
     .option('-r, --registry [url]', '默认是 https://registry.npmjs.org, 可以只用快手私服: https://npm.corp.kuaishou.com')
     .description('初始化脚手架工具')
-    .action(options => {
-        choice.init(options);
+    .action(async () => {
+        const configContent = await configModel.get();
+        choice.init(configContent);
     });
+
+program
+    .command('config')
+    .option('--get', '获取配置文件')
+    .option('--set', '设置配置文件')
+    .description('配置config项')
+    .action(options => {
+        configModel.init(options);
+    });
+
+// 如果没有传参数，显示help
+if (process.argv.length <= 2) {
+    shell.exec('node ./bin/cli.js -h');
+}
 
 program.parse(process.argv);
